6c4913998264999fd514a4bd9932c5b3cfcda3f7
ALUMNADO\rebolo <reginoboschlozano@gmail.com>
2017-12-13 13:11:08 +0100

    SUbida Clase

--- Programacion/JS/Bingo/bingo.js
+++ Programacion/JS/Bingo/bingo.js
@@ -2,13 +2,11 @@
   Autor : Regi
   Proyecto Bingo
 */
-
 //array numeros
 var arrayNumeros=[0];
 // Crear nodo de tipo Element numeros bingo
 var nodoBoton_padre = document.getElementById("contenido");
 var nodoBoton_plantilla = document.getElementById("boton_");
-
 // Crear nodo de tipo Element numeros carton
 var nodoBoton_padre_carton = document.getElementById("contenido_0");
 var nodoBoton_plantilla_carton = document.getElementById("boton_carton_");
@@ -23,17 +21,30 @@ var arrayNodos_Carton = new Array(8);
 var array_num_ale_carton=[0];
 //array numeros aleaotorios Bingo
 var array_num_bingo=[0];
+//linea
+var linea = false;
+//bingo
+var bingo = false;
+//contador premios
+var contador = 0;
 
-
-
-function RellenamosArraysBingo()
+function Rand_ArrayCarton()
 {
-  let contador = 0;
-//  num_ale= Math.round(Math.random() * (90 - 0) + 0);
-  for (var i = 0; i < 91; i++)
+  let _random_Num=[0];
+
+  for (let i = 0; i < 10; i++)
   {
-      array_num_bingo[i]=i;
+    if(i<4)
+    _random_Num[i] = 1;
+    else
+    {
+      _random_Num[i] = 0;
+    }
   }
+  //los ordenamos al azar
+ _random_Num.sort(() => Math.random() > 0.5 ? 1 : -1);
+
+  return _random_Num;
 }
 
 function RellenamosArraysCarton()
@@ -49,6 +60,15 @@ function RellenamosArraysCarton()
     InitCarton(array_num_ale_carton);
 }
 
+function RellenamosArraysBingo()
+{
+  for (var i = 1; i < 92; i++)
+  {
+      array_num_bingo[i-1]=i;
+  }
+  array_num_bingo.sort();
+}
+
 function InitBolasBingo()
 {
   //eliminamos el boton inicial del html
@@ -56,17 +76,18 @@ function InitBolasBingo()
   a.removeChild(nodoBoton_plantilla_carton);
 
       //añadimos al array los clones de los botones por cada posición en el array un clon
-      for(let i=1; i<91; i++)
+      for(let i=0; i<91; i++)
       {   arrayNodos_BolasBingo[i]=nodoBoton_plantilla.cloneNode(true);
           arrayNodos_BolasBingo[i].id="boton_"+i;
-          arrayNodos_BolasBingo[i].value = "   ";
-          //añade los botones
+          arrayNodos_BolasBingo[i].value = array_num_bingo[i];
           nodoBoton_padre.appendChild(arrayNodos_BolasBingo[i]);
       }
 }
 
 function InitCarton(_arrayCarton)
 {
+  //random posiciones carton sin numeros
+  let random_Num=[0];
   //eliminamos el boton inicial del html
   let a = nodoBoton_plantilla.parentNode;
   a.removeChild(nodoBoton_plantilla);
@@ -76,20 +97,27 @@ function InitCarton(_arrayCarton)
     arrayNodos_Carton[i]=new Array(8);
   }
 
-
-
+random_Num=Rand_ArrayCarton();
   //añadimos al array los clones de los botones por cada posición en el array un clon
   for (let i=0; i<1; i++)
   {
     for (let e=0; e<9; e++)
       {
+          num =random_Num[e];
           arrayNodos_Carton[i][e] = nodoBoton_plantilla_carton.cloneNode(true);
-          arrayNodos_Carton[i][e].id="boton_carton_"+i+"_"+e;
-          arrayNodos_Carton[i][e].value = _arrayCarton[e];
+          arrayNodos_Carton[i][e].id=i+"_"+e;
+          if(random_Num[e] == 0)
+            arrayNodos_Carton[i][e].value = _arrayCarton[e];
+          else
+          {
+            arrayNodos_Carton[i][e].value ="";
+          }
           nodoBoton_padre_carton.id="contenido_0";
           nodoBoton_padre_carton.appendChild(arrayNodos_Carton[i][e]);
       }
   }
+  random_Num=Rand_ArrayCarton();
+
   //añadimos al array los clones de los botones por cada posición en el array un clon
   for (let i=1; i<2; i++)
   {
@@ -97,11 +125,17 @@ function InitCarton(_arrayCarton)
       {
           arrayNodos_Carton[i][e] = nodoBoton_plantilla_carton.cloneNode(true);
           arrayNodos_Carton[i][e].id="boton_carton_"+i+"_"+e;
-          arrayNodos_Carton[i][e].value = _arrayCarton[e+9];
+          if(random_Num[e] == 0)
+              arrayNodos_Carton[i][e].value = _arrayCarton[e+7];
+          else
+          {
+            arrayNodos_Carton[i][e].value ="";
+          }
           nodoBoton_padre_carton.id="contenido_1";
           nodoBoton_padre_carton_1.appendChild(arrayNodos_Carton[i][e]);
       }
   }
+  random_Num=Rand_ArrayCarton();
   //añadimos al array los clones de los botones por cada posición en el array un clon
   for (let i=2; i<3; i++)
   {
@@ -109,20 +143,54 @@ function InitCarton(_arrayCarton)
       {
           arrayNodos_Carton[i][e] = nodoBoton_plantilla_carton.cloneNode(true);
           arrayNodos_Carton[i][e].id="boton_carton_"+i+"_"+e;
-          arrayNodos_Carton[i][e].value = _arrayCarton[e+18];
+          if(random_Num[e] == 0)
+            arrayNodos_Carton[i][e].value = _arrayCarton[e+16];
+          else
+          {
+            arrayNodos_Carton[i][e].value ="";
+          }
           nodoBoton_padre_carton.id="contenido_2";
           nodoBoton_padre_carton_2.appendChild(arrayNodos_Carton[i][e]);
       }
   }
-
 }
 
 function Comprobar(_value,_id)
 {
-  console.log("HOLAAAA");
-  console.log(_value+"_"+_id);
+  let encontradaBola=false;
+
+  for (let i = 0; i < arrayNodos_BolasBingo.length; i++)
+  {
+      if(arrayNodos_BolasBingo[i].value == _value)
+      {
+        encontradaBola = true;
+      }
+  }
+  if(encontradaBola == true)
+    {
+      OpcionesBuscar(_id);
+    }
 }
 
+function OpcionesBuscar(_value)
+{
+  let i=0;
+  let j=0;
+  let num=false;
+  i = _value.charAt(0);
+  j = _value.charAt(2);
+
+  arrayNodos_Carton[i][j] =0;
+
+  for (let e = 0; e < arrayNodos_Carton.length; i++)
+  {
+    if(arrayNodos_Carton[0][e] != 0)
+      num = false;
+      else {
+        num=true;
+      }
+  }
+}
 
 RellenamosArraysBingo();
 RellenamosArraysCarton();
