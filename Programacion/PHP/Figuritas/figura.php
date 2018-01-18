488622ad8cd648b57e9437b09e93def289695149
regi <reginoboschlozano@gmail.com>
2018-01-18 09:05:37 +0100

    SUbida PHP Eliminado

---
+++ Programacion/PHP/Figuritas/figura.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>FIGURA 1</title>
+  </head>
+  <body>
+
+    <?php include ("Figura_1.php"); ?>
+
+  </body>
+</html>
---
+++ Programacion/PHP/prueba_index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Prueba html - php</title>
+  </head>
+  <body>
+    <h1>Prueba html - php</h1>
+
+        <?php
+          $nombre="Regi";
+          echo $nombre;
+        ?>
+
+  </body>
+</html>
