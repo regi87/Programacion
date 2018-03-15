5fdcdf2f59b10f452793541eac7b892ade6f84f2
regi <reginoboschlozano@gmail.com>
2018-03-15 10:58:43 +0100

    Subida Act 8 fallo MSQLI

---
+++ Programacion/newProject/public/index.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+use Daw\models\Table as Table;
+use Daw\Config\Config;
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Pagina home del proyecto</title>
+  </head>
+  <body>
+    <?php
+    //Ejemplo de uso de modelos
+    $t=new Table();
+    $c=new Config();
+
+    ?>
+  </body>
+</html>
---
+++ Programacion/newProject/public/listaUsuarios.php
@@ -0,0 +1,14 @@
+<?php
+  require_once __DIR__.'/../vendor/autoload.php'
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>Listador Usuarios</title>
+   </head>
+   <body>
+
+   </body>
+ </html>
