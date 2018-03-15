bee3c04662af72cd2db8e88f9947ac3156f28d54
regi <reginoboschlozano@gmail.com>
2018-03-15 12:23:46 +0100

    Activadad 8 15/03 -Subida

--- Programacion/Proyecto_8/public/index.php
+++ Programacion/Proyecto_8/public/index.php
@@ -1,9 +1,8 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
-use Daw\models\Table as Table;
-use Daw\Config\Config;
-
+use Daw\models\classes\Usuario;
 ?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -12,9 +11,23 @@ use Daw\Config\Config;
   </head>
   <body>
     <?php
+    //Creamos el objeto Usuario
+    $usuario=new Usuario();
+    //Para insetar/actualizar/eliminar usuario se deben descomentar !!
+    //Insertar Usuario
+        //$usuario->insertUsuario("Carlos","1985-2-12","Ramos Bosch",4,10,"ramos@ramos.com","ramos","ramos10");
+    //Actualizar Usuario
+      //$usuario->updateUsuario("Carlos","Carlos","1990-2-12","Bosch",5,12,"ramos@bosch.com","ramos30","ramos30");
+    //Elimnar Usuario
+      //$delete=$usuario->deleteUsuario("Carlos");
     //Ejemplo de uso de modelos
-    $t=new Table();
-    $c=new Config();
+    //Listado Usuarios
+        $resultado=$usuario->findAllUsuario();
+
+        foreach ($resultado as $usuario)
+        {
+          echo "Nombre: ".$usuario["nombre"]."  ".$usuario["apellidos"]."<br>";
+        }
 
     ?>
   </body>
